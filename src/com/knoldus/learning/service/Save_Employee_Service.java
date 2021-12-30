fdb805131e5a9d8a1f38a0711af5702d2162de46
Shashikant Tanti <88486204+Shashikant-15@users.noreply.github.com>
2021-12-30 17:30:51 +0530

    Update Save_Employee_Service.java

--- src/com/knoldus/learning/service/Save_Employee_Service.java
+++ src/com/knoldus/learning/service/Save_Employee_Service.java
@@ -3,6 +3,10 @@ package com.knoldus.learning.service;
 import com.knoldus.learning.entity.Employee;
 
 public interface Save_Employee_Service {
+    /**
+     * used to Save Employee Services
+     * @param employee arguments passes to method for saveEmployee
+     */
     void saveEmployee(Employee employee);
 
     int getEmployeeCount();
