c23d4725eefd1d865cdb21289d466f2f8a1c53fd
Shashikant Tanti <88486204+Shashikant-15@users.noreply.github.com>
2021-12-30 17:28:41 +0530

    Update LetsValidate_Employee_Service_Implement.java

--- src/com/knoldus/learning/service/LetsValidate_Employee_Service_Implement.java
+++ src/com/knoldus/learning/service/LetsValidate_Employee_Service_Implement.java
@@ -1,13 +1,20 @@
 package com.knoldus.learning.service;
 
 import com.knoldus.learning.entity.Employee;
-
+/**
+ * Represents a Employee Service Vallidation in EmployeeService.
+ * A ClassRoom can have different entity such as roomId.
+ */
 public class LetsValidate_Employee_Service_Implement implements LetsValidate_EmployeeService {
+    /**
+     * used to Validate_Employee_Services
+     * @param e arguments passes to method for Employee
+     */
     @Override
     public boolean validateEmployee(Employee e) {
-        if(e.getId() <=0){
+        if( e.getId() <= 0){
             return false;
         }
-        return e.getName() != null && !e.getName().isEmpty();
+        return e.getName() != null && !e.getName().isEmpty(); // outcomes return
     }
 }
