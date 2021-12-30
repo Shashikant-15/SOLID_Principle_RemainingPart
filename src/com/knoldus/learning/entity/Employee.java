3a057675b4978e113c4263c45a016eb4c0d0d266
Shashikant Tanti <88486204+Shashikant-15@users.noreply.github.com>
2021-12-30 17:24:29 +0530

    Update Employee.java

--- src/com/knoldus/learning/entity/Employee.java
+++ src/com/knoldus/learning/entity/Employee.java
@@ -1,14 +1,25 @@
 package com.knoldus.learning.entity;
-
+/**
+ * Represents a Employee in the SOLID Principle.
+ * A Employee can have different entity such as Id,name etc.
+ */
 public class Employee {
+    
     int id;
     String name;
-
+    
+    /**
+     *  Creates a new Employee with the given id & name..
+     * @param i  This Employee's new ID. 
+     * @param num This Employee's new name. 
+     */
     public Employee(int i, String num) {
         this.id=i;
         this.name=num;
     }
-
+      /**
+     * Used getter and setter for Employee 
+     */
     public int getId() {
         return id;
     }
