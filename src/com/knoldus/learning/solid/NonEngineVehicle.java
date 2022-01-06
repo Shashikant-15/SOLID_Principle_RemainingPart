6bad2ddcf8c7250f9689343e23c7fa7f2e8a1465
Shashikant Tanti <88486204+Shashikant-15@users.noreply.github.com>
2022-01-06 21:57:08 +0530

    Update NonEngineVehicle.java

--- src/com/knoldus/learning/solid/NonEngineVehicle.java
+++ src/com/knoldus/learning/solid/NonEngineVehicle.java
@@ -1,6 +1,9 @@
 package com.knoldus.learning.solid;
-
+     /**
+     * Represents a Vehicle without engine
+     */
 public class NonEngineVehicle extends Vehicle {
+    
     public void startEngine() throws Exception {
         super.startEngine();
     };
