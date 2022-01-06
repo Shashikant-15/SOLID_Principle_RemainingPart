eef6d1e28ea7aab0099bf634a8b33f2a16c742b2
Shashikant Tanti <88486204+Shashikant-15@users.noreply.github.com>
2022-01-06 21:43:40 +0530

    Update Save_Employee_Service_Implement.java

--- src/com/knoldus/learning/service/Save_Employee_Service_Implement.java
+++ src/com/knoldus/learning/service/Save_Employee_Service_Implement.java
@@ -1,18 +1,27 @@
 package com.knoldus.learning.service;
 
 import com.knoldus.learning.entity.Employee;
-
 import java.util.HashMap;
 import java.util.Map;
 
+    /**
+    *To implement Save_Employee_Service with Save_Employee_Service_Implement class
+    */
 public class Save_Employee_Service_Implement implements Save_Employee_Service {
 
     Map<Integer,Employee> employeeMap = new HashMap<>();
+    
+    /**
+     * Creates a new saveEmployee to save employee details.
+     * @param employee associated with Save employee 
+     */
     @Override
     public void saveEmployee(Employee employee) {
         employeeMap.put(employee.getId(),employee);
     }
-
+    /**
+    *To get Numbers of employed 
+    */
     @Override
     public int getEmployeeCount() {
         return employeeMap.size();
