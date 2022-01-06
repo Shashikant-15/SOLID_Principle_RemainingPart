993b8e7d74ccdd9ad2034c3c92dcb61bac579f46
Shashikant Tanti <88486204+Shashikant-15@users.noreply.github.com>
2022-01-06 21:56:05 +0530

    Update EngineVehicle.java

--- src/com/knoldus/learning/solid/EngineVehicle.java
+++ src/com/knoldus/learning/solid/EngineVehicle.java
@@ -1,6 +1,12 @@
 package com.knoldus.learning.solid;
-
+     /**
+     * Represents a Vehicle having startable engine.
+     * A EngineVehicle can have different tyes like diesel,petrol etc.
+     */
 public class EngineVehicle extends Vehicle{
+    /**
+     * Represents a Engine to start.
+     */
     public void startEngine() throws Exception {
         super.startEngine();
     };
